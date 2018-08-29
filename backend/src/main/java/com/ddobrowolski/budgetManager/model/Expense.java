4328a370a866e8db86096857008975fb6d27f795
DDobrowolski <damian.dobrowolski99@gmail.com>
2018-08-29 14:05:42 +0200

    Tests and code refactoring

--- backend/src/main/java/com/ddobrowolski/budgetManager/model/Expense.java
+++ backend/src/main/java/com/ddobrowolski/budgetManager/model/Expense.java
@@ -1,7 +1,6 @@
 package com.ddobrowolski.budgetManager.model;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- backend/src/test/java/com/ddobrowolski/budgetManager/controller/UserControllerUTest.java
+++ backend/src/test/java/com/ddobrowolski/budgetManager/controller/UserControllerUTest.java
@@ -1,24 +1,24 @@
 package com.ddobrowolski.budgetManager.controller;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.math.BigDecimal;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.ddobrowolski.budgetManager.model.User;
 import com.ddobrowolski.budgetManager.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserControllerUTest {
 	
@@ -55,5 +55,55 @@ public class UserControllerUTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(userJson))
                 .andExpect(status().isOk());
+	}
+	
+	@Test
+	void createUser_shouldFailOnPasswordValidation() throws Exception {
+		User user = new User();
+		user.setUsername("username");
+		user.setEmail("test@test.com");
+		user.setMonthBudget(new BigDecimal(0));
+		
+		 ObjectMapper mapper = new ObjectMapper();
+	     String userJson = mapper.writeValueAsString(user);
+	     
+	     given(userService.save(user)).willReturn(user);
+	        
+	     mvc.perform(post("/register")
+	               	.contentType(MediaType.APPLICATION_JSON)
+	                .content(userJson))
+	                .andExpect(status().is4xxClientError());
+	}
+	
+	@Test
+	void createUser_shouldFailOnUsernameValidation() throws Exception {
+		User user = new User();
+		user.setPassword("password");
+		user.setEmail("test@test.com");
+		user.setMonthBudget(new BigDecimal(0));
+		
+		 ObjectMapper mapper = new ObjectMapper();
+	     String userJson = mapper.writeValueAsString(user);
+	     
+	     mvc.perform(post("/register")
+	               	.contentType(MediaType.APPLICATION_JSON)
+	                .content(userJson))
+	                .andExpect(status().is4xxClientError());
+	}
+	
+	@Test
+	void createUser_shouldFailOnEmailValidation() throws Exception {
+		User user = new User();
+		user.setPassword("password");
+		user.setUsername("username");
+		user.setMonthBudget(new BigDecimal(0));
+		
+		 ObjectMapper mapper = new ObjectMapper();
+	     String userJson = mapper.writeValueAsString(user);
+	     
+	     mvc.perform(post("/register")
+	               	.contentType(MediaType.APPLICATION_JSON)
+	                .content(userJson))
+	                .andExpect(status().is4xxClientError());
 	}
 }
